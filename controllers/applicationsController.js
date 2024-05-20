23f47af7808ca79a1a72d9426e4a93ea6bc46761
Nugget-Meister <tremainecamacho@gmail.com>
2024-05-20 12:14:15 -0400

    boilerplate for update/delete

--- controllers/applicationsController.js
+++ controllers/applicationsController.js
@@ -19,7 +19,7 @@ applications.get("/", (request, response) => {
         })
     }catch(err){
         response.status(404).json({
-            error: error
+            error : err
         })
     }
 })
@@ -27,21 +27,47 @@ applications.get("/", (request, response) => {
 applications.get("/:id", (req, res) => {
     let { id } = req.params
 
-    try{}catch(err){}
-    let application = getApplicationById(id)
+    try{
+        let application = getApplicationById(id)
+        res.status(200).json({
+            data : application
+        })
+    }catch(err){
+        res.status(404).json({
+            error : err
+        })
+    }
 
 })
 
 applications.post("/", (req, res) => {
+ try {
+    let application = createApplication(req.body)
 
+ } catch (err){
+    res.status(500).json({
+        error: err
+    })
+ }
 })
 
 applications.put('/:id', (req, res) => {
-
-})
+    let { id } = req.params;
+    try {
+        let application = updateApplication(id, req.body)
+        res.status(200)
+    } catch(err){}
+    })
 
 applications.delete("/:id", (req, res) => {
-
+    let { id } = req.params
+    try{
+        let application = deleteApplication(id)
+    }catch(err){
+        res.status(404).json({
+            error: err
+        })
+    }
 })
 
 module.exports =  applications 
